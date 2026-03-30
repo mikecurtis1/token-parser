4a107c6cfdc3fe3489ebd9c6705b25250ddfde4a
Mike Curtis <mikecurtis1@gmail.com>
2026-03-30 18:01:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+# ---- CLI ENTRY POINT ----
 
+def main():
+    if len(sys.argv) > 1:
+        q = ' '.join(sys.argv[1:])
+    else:
+        q = sys.stdin.read().strip()
+
+    tokenizer = Tokenizer()
+    tokens = tokenizer.tokenize(q)
+
+    for t in tokens:
+        print(t)
+
+
+if __name__ == "__main__":
+    main()
